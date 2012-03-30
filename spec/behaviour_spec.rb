1b04d254439e413dc2fcfabbcb1fb4101d368d1f
hugomarinho <hugomarinho@nexus-core.(none)>
2012-03-30 12:13:50 +0100

    some behaviour tests, added support for 92 numbers in PT

--- spec/behaviour_spec.rb
+++ spec/behaviour_spec.rb
@@ -4,5 +4,22 @@ describe Behaviour do
   it "should have tests" do
     pending "write tests or I will kneecap you"
   end 
+
+  describe ".pt_checkphoneid" do
+    #vodafone_numbers = ["912345678","+351912345678"]
+    it "should return vodafone when given 91 followed by 7 random digits" do
+      Behaviour.pt_checkphoneid((910000000 + rand(9999999)).to_s).should eq("vodafone")
+      
+    end
+    it "should return tmn when given 96 followed by 7 random digits" do
+      Behaviour.pt_checkphoneid((960000000 + rand(9999999)).to_s).should eq("tmn")
+    end
+    it "should return tmn when given 92 followed by 7 random digits" do
+      Behaviour.pt_checkphoneid((920000000 + rand(9999999)).to_s).should eq("tmn")
+    end
+    it "should return optimus when given 93 followed by 7 random digits" do
+      Behaviour.pt_checkphoneid((930000000 + rand(9999999)).to_s).should eq("optimus")
+    end
+  end
 end
 
